ea42067e287cbbe65b4302acbf199053a97654b9
giza-data-team <gizadatateam@gmail.com>
2023-12-10 15:19:37 +0200

    Add setup.py and MIT LICENSE

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+print(requirements)
+
+setup(
+    name='GizaML',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    license='MIT',
+)
